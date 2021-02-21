cf6170f306b60a97f451400de23652151b059fa9
Rob Silverman <robert.silverman@willowtreeapps.com>
2021-02-20 20:45:04 -0500

    first pass at landscape layout constraints

--- NameGame-UIKit/ViewController.swift
+++ NameGame-UIKit/ViewController.swift
@@ -22,25 +22,49 @@ class ViewController: UIViewController {
         targetImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -50),
         targetImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50),
         targetImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 71),
+        
         logoImage.centerXAnchor.constraint(equalTo: targetImage.centerXAnchor),
         logoImage.centerYAnchor.constraint(equalTo: targetImage.centerYAnchor),
         logoImage.heightAnchor.constraint(equalTo: targetImage.heightAnchor, multiplier: 0.67),
+        logoImage.widthAnchor.constraint(equalTo: targetImage.widthAnchor, multiplier: 0.67),
+        
         timedModeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         timedModeButton.heightAnchor.constraint(equalToConstant: 56),
         timedModeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
         timedModeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+        
         practiceModeButton.bottomAnchor.constraint(equalTo: timedModeButton.topAnchor, constant: -8),
-        practiceModeButton.heightAnchor.constraint(equalToConstant: 56),
-        practiceModeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-        practiceModeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+        practiceModeButton.heightAnchor.constraint(equalTo: timedModeButton.heightAnchor),
+        practiceModeButton.widthAnchor.constraint(equalTo: timedModeButton.widthAnchor),
+        
         instructionLabel.bottomAnchor.constraint(equalTo: practiceModeButton.topAnchor, constant: -55),
         instructionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 47),
         instructionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -47)
     ]}
     
     var landscapeConstraints: [NSLayoutConstraint] {[
-        targetImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -10),
-        targetImage.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        targetImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -50),
+        targetImage.topAnchor.constraint(equalTo: view.topAnchor, constant: -50),
+        targetImage.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 50),
+        targetImage.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: view.frame.width * -0.25),
+        
+        logoImage.centerXAnchor.constraint(equalTo: targetImage.centerXAnchor),
+        logoImage.centerYAnchor.constraint(equalTo: targetImage.centerYAnchor),
+        logoImage.heightAnchor.constraint(equalTo: targetImage.heightAnchor, multiplier: 0.67),
+        logoImage.widthAnchor.constraint(equalTo: targetImage.widthAnchor, multiplier: 0.67),
+        
+        timedModeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        timedModeButton.heightAnchor.constraint(equalToConstant: 56),
+        timedModeButton.widthAnchor.constraint(equalToConstant: 359),
+        timedModeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -64),
+        
+        practiceModeButton.bottomAnchor.constraint(equalTo: timedModeButton.topAnchor, constant: -8),
+        practiceModeButton.heightAnchor.constraint(equalTo: timedModeButton.heightAnchor),
+        practiceModeButton.widthAnchor.constraint(equalTo: timedModeButton.widthAnchor),
+        practiceModeButton.trailingAnchor.constraint(equalTo: timedModeButton.trailingAnchor),
+        
+        instructionLabel.bottomAnchor.constraint(equalTo: practiceModeButton.topAnchor, constant: -55),
+        instructionLabel.widthAnchor.constraint(equalToConstant: 281)
     ]}
     
     override func viewDidLoad() {

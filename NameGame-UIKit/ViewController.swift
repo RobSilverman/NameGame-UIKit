5e0380fa7bfbdf506022c6353d3123200db69cf3
Rob Silverman <robert.silverman@willowtreeapps.com>
2021-02-20 19:00:52 -0500

    call layoutViews() in viewdidload; setup target + logo constraints in new framework

--- NameGame-UIKit/ViewController.swift
+++ NameGame-UIKit/ViewController.swift
@@ -17,19 +17,27 @@ class ViewController: UIViewController {
     @IBOutlet var practiceModeButton: UIButton!
     @IBOutlet var timedModeButton: UIButton!
     
-    let portraitConstraints: [NSLayoutConstraint] = [
-        
-    ]
+    var portraitConstraints: [NSLayoutConstraint] {[
+        targetImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        targetImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -50),
+        targetImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50),
+        targetImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 118),
+        logoImage.centerXAnchor.constraint(equalTo: targetImage.centerXAnchor),
+        logoImage.centerYAnchor.constraint(equalTo: targetImage.centerYAnchor)
+    ]}
     
-    let landscapeConstraints: [NSLayoutConstraint] = [
-        
-    ]
+    var landscapeConstraints: [NSLayoutConstraint] {[
+        targetImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -10),
+        targetImage.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ]}
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         practiceModeButton.layer.cornerRadius = 10
         timedModeButton.layer.cornerRadius = 10
+        
+        layoutViews(UIDevice.current.orientation)
     }
     
     override func viewWillAppear(_ animated: Bool) {

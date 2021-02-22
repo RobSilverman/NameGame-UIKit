8f557b5ef92b22108af0237410bd5e4a9a6d1457
Rob Silverman <robert.silverman@willowtreeapps.com>
2021-02-22 18:45:09 -0500

    draw from top of circle instead of right side

--- NameGame-UIKit/TimerView.swift
+++ NameGame-UIKit/TimerView.swift
@@ -15,19 +15,18 @@ class TimerView: UIView {
     override func draw(_ rect: CGRect) {
         if let context = UIGraphicsGetCurrentContext() {
             context.setLineWidth(2)
-            UIColor.black.set()
-            UIColor(named: "MenuButtonColor")?.set()
+            UIColor.lightGray.set()
             
             let center = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
             let radius = (frame.size.width - 5)/2
             
-            //Draw completed (blue) section
-            context.addArc(center: center, radius: radius, startAngle: 0.0, endAngle: .pi * 2.0 * CGFloat(currentProgress), clockwise: true)
+            //Draw incomplete (light gray) circle
+            context.addArc(center: center, radius: radius, startAngle: -(0.5 * .pi), endAngle: .pi * 2.0 * CGFloat(currentProgress) - (0.5 * .pi), clockwise: true)
             context.strokePath()
             
-            //Draw incomplete (light gray) section
-            UIColor.lightGray.set()
-            context.addArc(center: center, radius: radius, startAngle: .pi * 2.0 * CGFloat(currentProgress), endAngle: .pi * 2.0, clockwise: true)
+            //Draw complete (blue) section overtop
+            UIColor(named: "MenuButtonColor")?.set()
+            context.addArc(center: center, radius: radius, startAngle: .pi * 2.0 * CGFloat(currentProgress) - (0.5 * .pi), endAngle: .pi * 2.0 - (0.5 * .pi), clockwise: true)
             context.strokePath()
         }
     }
